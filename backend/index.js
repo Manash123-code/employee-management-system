16853278cb1ca4bb5cb8d0d0d23d3833bf5f8169
Manash123-code <manashpratimsaikia561@gmail.com>
2026-08-14 21:17:58 +0530

    push actual backend files

---
+++ backend/index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+require('dotenv').config();
+const cors = require('cors');
+const EmployeeRoutes = require('./Routes/EmployeeRoutes');
+const PORT = process.env.PORT || 8080;
+app.get('/',(req,res)=>{
+    res.send('Employee Mgm server is running');
+});
+
+require('./Models/db');
+app.use(cors());
+app.use(bodyParser.json());
+
+app.use('/api/employees', EmployeeRoutes);
+
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT: ${PORT}`);
+})
